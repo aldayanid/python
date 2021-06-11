41222bf5cfdbc9d2c4530a85b3891eab5a40fb67
Dani <aldayanid@gmail.com>
2021-06-11 19:28:03 +0300

    Trying to code IF or Func. Added new .py file: ytless1

--- lesson1.py
+++ lesson1.py
@@ -1,7 +1,20 @@
 #!/usr/bin/env python3
+#-*- coding: utf-8 -*-
 #F to C conversion
-print("Enter the temperature in Farenheit:")
-far=input ()
-degree_sign = u"\N{DEGREE SIGN}"
-cel=(far-32)/1.8
-print("In Celsius", cel,degree_sign)
+#print("Enter the temperature in Farenheit:")
+#out_cel=(in_far-32)/1.8
+#out_far=(in_cel+32)*1.8
+
+corf=input ("Celsius or Farenheit:")
+    if corf == "F":
+        print("Enter the temperature in Farenheit:")
+        in_far=input()
+        out_cel=(int(in_far)-32)/1.8
+        print("In Celsius is: " + str(out_cel)+"°C.")
+    elif corf == "C":
+        print("Enter the temperature in Celsius:")
+        in_cel=input()
+        out_far=(int(in_cel)+32)*1.8
+        print("In Fahrenheit is: " + str(out_far)+"°F.")
+    else:
+        print ("Error")
---
+++ ytless1.py
@@ -0,0 +1,7 @@
+#birth_year = input ('Birth year: ')
+#age = 2021 - int(birth_year)
+#print(age)
+
+lbs = input ('Input pounds: ')
+kilo = int(lbs)*0.45359237
+print (round(kilo, 2))
