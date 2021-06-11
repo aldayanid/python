aae1d7726b81a9c7f0d0753456839e65eb210735
Dani <aldayanid@gmail.com>
2021-06-11 10:24:33 +0300

    Started pythoning. Fahrenheiet to Celsius converter

---
+++ lesson1.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+#F to C conversion
+print("Enter the temperature in Farenheit:")
+far=input ()
+degree_sign = u"\N{DEGREE SIGN}"
+cel=(far-32)/1.8
+print("In Celsius", cel,degree_sign)
