1dc08ba10fe937f9c8b1e3f99ff6d5c380f772fc
Dani <aldayanid@gmail.com>
2021-06-12 12:55:58 +0300

    Celsius-Fahrenheit converter works well

--- lesson1.py
+++ lesson1.py
@@ -6,15 +6,26 @@
 #out_far=(in_cel+32)*1.8
 
 corf=input ("Celsius or Farenheit:")
-    if corf == "F":
+if corf == "F" or corf == "f":
         print("Enter the temperature in Farenheit:")
-        in_far=input()
+        in_far=float(input())
         out_cel=(int(in_far)-32)/1.8
-        print("In Celsius is: " + str(out_cel)+"°C.")
-    elif corf == "C":
+        print("In Celsius is: " + str(round(out_cel, 2))+"°C.")
+elif corf == "C":
         print("Enter the temperature in Celsius:")
-        in_cel=input()
+        in_cel=float(input())
         out_far=(int(in_cel)+32)*1.8
-        print("In Fahrenheit is: " + str(out_far)+"°F.")
-    else:
-        print ("Error")
+        print("In Fahrenheit is: " + str(round(out_far, 2))+"°F.")
+else:
+        print("Error")
+
+
+# print("For Celsius to Fahrenheit type C/c \n or F/f for Fahrenheit to Celsius"
+# if input() == C or input() == c:
+#     FtoC = input ("Enter the temperature in Farenheit:")
+#     out_C=(int(FtoC)-32)/1.8
+#     print(f"Result in Celsius is: ${out_C}°C.")
+# else:
+#     CtoF = input ("Enter the temperature in Celsius:")
+#     out_F=(int(CtoF)+32)*1.8
+#     print(f"Result in Celsius is: ${out_F}°C.")
